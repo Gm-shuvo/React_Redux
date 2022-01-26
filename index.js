4c5775314ffdc367748433b7b334ee04793eb360
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2022-01-26 14:11:15 +0600

    Combained two reducer

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+
 // actions 
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 const buyCake = () => {
   return {
@@ -11,13 +14,24 @@ const buyCake = () => {
   }
 }
 
+const buyIceCreame = () => {
+  return {
+    type: BUY_ICECREAM,
+    payload: 'Redux action'
+  }
+}
+
 // reducer
 
-const initialState = {
+const CakeinitialState = {
   numberOfCake: 10
 }
 
-const reducer = (state = initialState, action) => {
+const IceCreameinitialState = {
+  numberOfIceCreame: 13
+}
+
+const cakeReducer = (state = CakeinitialState, action) => {
   switch (action.type) {
     case BUY_CAKE: return {
       ...state,
@@ -30,16 +44,41 @@ const reducer = (state = initialState, action) => {
   }
 }
 
+const iceCreamReducer = (state = IceCreameinitialState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM: return {
+      ...state,
+      numberOfIceCreame: state.numberOfIceCreame - 1
+    }
+      break;
+  
+    default: return state
+      break;
+  }
+}
+
+// combaine multiple reducer with combaine reducer
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCreame: iceCreamReducer
+})
+
 // CreateStore 
 
-const store = createStore(reducer)
+const store = createStore(rootReducer)
 
 console.log('Initial value', store.getState());
 
 const unsubscribed = store.subscribe(() => console.log('Update value', store.getState()))
+//dispatch buycake
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
 
-store.dispatch(buyCake())
-store.dispatch(buyCake())
+// dispatch 
+store.dispatch(buyIceCreame())
+store.dispatch(buyIceCreame())
+store.dispatch(buyIceCreame())
 unsubscribed()
-store.dispatch(buyCake())
 
